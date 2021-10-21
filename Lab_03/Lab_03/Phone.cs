63a672975fc80abc34f1c83c47474ae0adf3df78
OlyaNikonchuk <olya.nikonchuk02@gmail.com>
2021-10-22 02:35:33 +0300

    2 try

--- Lab_03/Lab_03/Phone.cs
+++ Lab_03/Lab_03/Phone.cs
@@ -8,46 +8,46 @@ namespace Lab_03
 {
     partial class Phone
     {
-        int Id=0;
+        public readonly int Id;
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Middlename { get; set; }
         public string Adress { get; set; }
+        public int World;
+        public int City;
         public int Card;
         int balance;
 
+        //Свойства
         public int Balance
         {
-            get 
+            get
             {
                 return balance;
             }
             set
             {
-                if (value<0 && value > 100000000)
-                {
-                    Console.WriteLine("Ошибка баланса");
-                }
-                else
-                {
+                if (value > -1000 && value < 100000000)
                     balance = value;
-                }
             }
-
         }
 
-        
 
-        public Phone(string surname ="", string name= "", string middlename = "", string adress="", int card=0, int balance=0 )
+        //Конструктор
+        public Phone(string surname ="", string name= "", string middlename = "", string adress="", int card=0, int balance=0, int city=0, int world = 0)
         {
-            ++Id;
+            Id = this.GetHashCode();
             Surname = surname;
             Name = name;
             Middlename = middlename;
             Adress = adress;
             Card = card;
             Balance = balance;
+            City = city;
+            World = world;
+      
         }
+
 
         public void PrintInfo()
         {
@@ -56,9 +56,12 @@ namespace Lab_03
             Console.WriteLine("ФИО: " + Surname + " " + Name + " " + Middlename);
             Console.WriteLine("Номер карты: " + Card);
             Console.WriteLine("Баланс: " + balance + "$");
-            
+            Console.WriteLine("Внутригородские звонки: " + City + " минут");
+            Console.WriteLine("Межгородские звонки: " + World + " минут");
+
         }
 
+        //Методы расчета баланса
         public void Debit(int debit)
         {
             Balance = Balance + debit;
@@ -67,6 +70,11 @@ namespace Lab_03
         public void Credit(int credit)
         {
             Balance = Balance - credit;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Middlename: {Middlename}"+ Card+City+World ; ;
         }
     }
 }
--- Lab_03/Lab_03/Program.cs
+++ Lab_03/Lab_03/Program.cs
@@ -6,17 +6,46 @@ using System.Threading.Tasks;
 
 namespace Lab_03
 {
-
+  
    
     class Program
     {
         static void Main()
         {
-            Phone person1 = new Phone("Петров", "Петр", "Петрович", "Беларусская,21", 5499, 98765);
+            Phone person1 = new Phone("Петров", "Петр", "Петрович", "Беларусская,21", 5499, 98765, 30,15);
+            //person1.PrintInfo();
+            //person1.Debit(1000);
+            //person1.Credit(222);
             person1.PrintInfo();
-            person1.Debit(1000);
-            person1.Credit(222);
-            person1.PrintInfo();
+            Phone person2 = new Phone("Иванов", "Иван", "Иванович", "Беларусская,21", 7777, 36541,59,0);
+            person2.PrintInfo();
+            Phone person3 = new Phone("Романов", "Петр", "Алексеевич", "Беларусская,21", 1234, 1234567, 240,120);
+            person3.PrintInfo();
+
+            Console.WriteLine("________________________________");
+
+            Phone[] arr = new Phone[3];
+            arr[0] = new Phone("Петров", "Петр", "Петрович", "Беларусская,21", 5499, 98765);
+            arr[1] = new Phone("Иванов", "Иван", "Иванович", "Беларусская,21", 7777, 36541);
+            arr[2] = new Phone("Романов", "Петр", "Алексеевич", "Беларусская,21", 1234, 1234567);
+
+            List<Phone> phone = new List<Phone>();
+
+
+            foreach (Phone el in arr)
+            {
+                if (el.World > 0)
+
+                el.PrintInfo();
+
+            }
+
+
+
+
+
+
+
 
 
         }
